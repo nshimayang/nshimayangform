c9dbd985857d31f5bbfd1085517fa0a9c1b36fc5
nshimayang <yujinishiyama8+github@gmail.com>
2018-10-09 23:47:16 +0900

    Add variables.tf & put out Variables

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-#  access_key = "anaccesskey"
-#  secret_key = "asecretkey"
-  region     = "ap-northeast-1"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.region}"
 }
