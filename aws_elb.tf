c98376f6e15443038f3cea245bc9aa212a7c0167
nshimayang <yujinishiyama8+github@gmail.com>
2018-10-13 00:20:56 +0900

    Add ELB Setting TF File

---
+++ aws_elb.tf
@@ -0,0 +1,39 @@
+resource "aws_elb" "elb" {
+  name               = "${terraform.env}-elb"
+  instances                   = [
+      "${aws_instance.web-a.id}",
+      "${aws_instance.web-c.id}"
+  ]
+  subnets = [
+      "${aws_subnet.public_a.id}",
+      "${aws_subnet.public_c.id}"
+  ]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    target              = "HTTP:80/index.html"
+  }
+
+  security_groups = [
+      "${aws_security_group.elb-sg.id}"
+  ]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 300
+  connection_draining         = true
+  connection_draining_timeout = 300
+
+  tags {
+    Name = "${terraform.env}-elb"
+    Environment = "${terraform.env}"
+  }
+}
