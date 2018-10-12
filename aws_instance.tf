66e898b23c639a7166cc7144ab0fdd011317c562
nshimayang <yujinishiyama8+github@gmail.com>
2018-10-12 23:23:34 +0900

    Add EC2 Instance In AZ-C & Data Source Get Most Recent AMI ID Refer,https://dev.classmethod.jp/cloud/aws/launch-ec2-from-latest-ami-by-terraform/

--- aws_instance.tf
+++ aws_instance.tf
@@ -3,7 +3,7 @@ data "template_file" "user_data" {
 }
 
 resource "aws_instance" "web-a" {
-  ami           = "ami-06cd52961ce9f0d85"
+  ami           = "${data.aws_ami.amazon_linux.id}"
   instance_type = "t2.micro"
   user_data = "${data.template_file.user_data.rendered}"
   subnet_id = "${aws_subnet.public_a.id}"
@@ -18,6 +18,26 @@ resource "aws_instance" "web-a" {
   }
   tags {
     Name = "${terraform.env}-web-az-a"
+    Environment = "${terraform.env}"
+  }
+}
+
+resource "aws_instance" "web-c" {
+  ami           = "${data.aws_ami.amazon_linux.id}"
+  instance_type = "t2.micro"
+  user_data = "${data.template_file.user_data.rendered}"
+  subnet_id = "${aws_subnet.public_c.id}"
+  associate_public_ip_address = true
+  key_name = "nshimayangform"
+  # iam_instance_profile = 
+  vpc_security_group_ids = ["${aws_security_group.web-sg.id}"]
+  root_block_device {
+      volume_type = "gp2"
+      volume_size = "30"
+      delete_on_termination = "true"
+  }
+  tags {
+    Name = "${terraform.env}-web-az-c"
     Environment = "${terraform.env}"
   }
 }
