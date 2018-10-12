834bb33afe98f44f339ae9e5bcb0f61bfb7cc743
nshimayang <yujinishiyama8+github@gmail.com>
2018-10-12 23:00:34 +0900

    Add Install MySQL Client

--- install_httpd.sh
+++ install_httpd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # for userdata
 
-/usr/bin/yum install -y httpd
+/usr/bin/yum install -y httpd mysql57
 /sbin/chkconfig httpd on
 /bin/echo "I'm `/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id`" > /var/www/html/index.html
 /sbin/service httpd start
